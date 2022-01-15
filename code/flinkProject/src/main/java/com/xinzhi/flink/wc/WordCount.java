18d26bd90ca575bfe4a2b2043245483bd61d9b32
1037847650@qq.com <liQAING21234>
2022-01-15 18:33:09 +0800

    flink worfCount 测试

--- code/flinkProject/src/main/java/com/xinzhi/flink/wc/WordCount.java
+++ code/flinkProject/src/main/java/com/xinzhi/flink/wc/WordCount.java
@@ -1,4 +1,11 @@
 package com.xinzhi.flink.wc;
 
+import org.apache.flink.api.java.ExecutionEnvironment;
+
 public class WordCount {
+    public static void main(String[] args) {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+
+    }
 }
