56530ba6bbe2d06933ce3414ba1dc8e03b4f0a3f
1037847650@qq.com <liQAING21234>
2022-03-14 15:47:32 +0800

    实时数仓项目启动

--- code/gmall2021/gmall2021-logger/src/main/java/com/example/gmall2021logger/controller/LoggerController.java
+++ code/gmall2021/gmall2021-logger/src/main/java/com/example/gmall2021logger/controller/LoggerController.java
@@ -1,14 +1,34 @@
 package com.example.gmall2021logger.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class LoggerController {
-    @RequestMapping("test")
-    public String test(){
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @RequestMapping("test1")
+    public String test1() {
         System.out.println("123213213");
         return "success";
     }
 
+    @RequestMapping("test2")
+    public String test2(@RequestParam("name") String name, @RequestParam("age") int age) {
+        System.out.println("name===>" + name + "  " + "age===>" + age);
+        return "success";
+    }
+
+    @RequestMapping("applog")
+    public String getLogger(@RequestParam("param") String jsonStr) {
+        log.info(jsonStr);
+        kafkaTemplate.send("ods_base_log", jsonStr);
+        return "success";
+    }
 }
---
+++ code/gmall2021/gmall2021-realtime/src/main/java/app/FlinkCDC.java
@@ -0,0 +1,48 @@
+package app;
+
+import com.alibaba.ververica.cdc.connectors.mysql.MySQLSource;
+import com.alibaba.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.alibaba.ververica.cdc.debezium.DebeziumSourceFunction;
+import com.alibaba.ververica.cdc.debezium.StringDebeziumDeserializationSchema;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class FlinkCDC {
+    //  获取执行环境
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env =
+                StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        env.enableCheckpointing(5000L);
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        env.getCheckpointConfig().enableExternalizedCheckpoints(
+                CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+        //2.4 指定从 CK 自动重启策略
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, 2000L));
+        //2.5 设置状态后端
+        env.setStateBackend(new FsStateBackend("hdfs://localhost:8020/flinkCDC"));
+        DebeziumSourceFunction<String> mysqlSource = MySQLSource.<String>builder()
+                .hostname("localhost")
+                .port(3306)
+                .username("root")
+                .password("3edcCFT^")
+                .databaseList("gmall2021")
+                .startupOptions(StartupOptions.initial())
+                .deserializer(new StringDebeziumDeserializationSchema())
+                .build();
+        //4.使用 CDC Source 从 MySQL 读取数据
+        DataStreamSource<String> mysqlDS = env.addSource(mysqlSource);
+        //5.打印数据
+        mysqlDS.print();
+        //6.执行任务
+        env.execute();
+
+
+
+
+    }
+}
